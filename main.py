86a49f57ce317dccd844cdd3174832cf50b386d7
thomas <s153012@student.dtu.dk>
2020-02-18 13:47:38 +0100

    Commented the plotting part

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ print("Number of electrons before cut:_ %.5f" %N_before)
 print("Number of electrons after cut: %.5f" %N_after)
 print("Total removed electrons: %.5f" %(N_before - N_after) )
 
-def electron3d(dataset, n):
-    values = dataset[n]['data']
+#def electron3d(dataset, n):
+#    values = dataset[n]['data']
 
 
-    print("Looking at " + str(dataset[n].atoms.symbols) )
-    mlab.contour3d(values, contours=50, transparent=True, vmin=-0.1)#, extent=[0, 198, 0, 198, 0, 198])
+#    print("Looking at " + str(dataset[n].atoms.symbols) )
+#    mlab.contour3d(values, contours=50, transparent=True, vmin=-0.1)#, extent=[0, 198, 0, 198, 0, 198])
 #    mlab.pipeline.volume(mlab.pipeline.scalar_field(values))
    
-electron3d(dataset, 5)
+#electron3d(dataset, 5)
