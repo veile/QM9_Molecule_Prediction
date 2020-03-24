adeefc273ff6ed52d34245bd655b65ac59c6ebe6
thomas <s153012@student.dtu.dk>
2020-03-24 13:25:46 +0100

    Adjusted for tar-file, adam optimizer and added time elapsed print

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from datetime import datetime
 
+start = datetime.now()
 
-data_dir = "Data/"
-dataset =  MolecularDataset(data_dir)
+tarfile = "qm9_000xxx_29.cube.tar.gz"
+dataset =  MolecularDataset(tarfile)
 
-train_set, test_set = torch.utils.data.random_split(dataset, [28, 0]) 
+train_set, test_set = torch.utils.data.random_split(dataset, [25, 3]) 
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, num_workers=0, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=1, num_workers=0, shuffle=False)
-
 
 # Training the Neural Network
 #Using CUDA if available
@@ -29,8 +30,8 @@ net.to(device)
 
 criterion = nn.CrossEntropyLoss()
 
-optimizer = optim.SGD(net.parameters(), lr=0.5)#, momentum=0.9)
-targets_tmp = np.zeros( (157, 157, 157) )
+optimizer = optim.Adam(net.parameters(), lr=1e-4)
+targets_tmp = np.zeros( (dataset.output_grid, dataset.output_grid, dataset.output_grid) )
 
 print("----Training begun----")
 print("[Epoch, Entry] - Loss")
@@ -70,6 +71,10 @@ for epoch in range(epochs):  # loop over the dataset multiple times
     print("Validation error after epoch %i is: %.3f" %(epoch+1, validation_error ))
         
 print('Finished Training')
-PATH = './QM9_net_%i_sigmoid.pth' %epochs
+PATH = './QM9_net.pth'
 print("Saving model to %s" %PATH)
 torch.save(net.state_dict(), PATH)
+
+print("Elapsed time:")
+elapsed = datetime.now() - start
+print(elapsed)
